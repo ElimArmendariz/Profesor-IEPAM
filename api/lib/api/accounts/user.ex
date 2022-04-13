adf126346b3e9c5ee3a87770e441d91a4c6e2737
korcline@hotmail.com <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>
2022-04-13 16:29:57 -0500

    Dependencies

---
+++ api/lib/api/accounts/user.ex
@@ -0,0 +1,25 @@
+defmodule Api.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :academiclevel, :string
+    field :address, :string
+    field :age, :string
+    field :email, :string
+    field :laboralxp, :string
+    field :name, :string
+    field :password, :string
+    field :phone, :string
+    has_many :userstrack, Api.Progress.UserTrack, on_delete: :delete_all
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :password, :name, :phone, :address, :age, :academiclevel, :laboralxp])
+    |> validate_required([:email, :password])
+  end
+end
