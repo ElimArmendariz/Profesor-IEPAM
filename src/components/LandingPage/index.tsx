dbf6a1a15d2241f92ae2455a73534d6a8d1554f4
korcline@hotmail.com <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>
2022-04-15 08:50:44 -0500

    Fixed login and register

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -15,9 +15,9 @@ import { Link } from "react-router-dom";
 const LandingPage = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [register, { loading: loadingRegister, error: errorRegister, data: dataRegister }] = useMutation(REGISTER);
-    const [login, { loading: loadingLogin, error: errorLogin, data: dataLogin }] = useMutation(LOGIN);
-
+    const [register] = useMutation(REGISTER);
+    const [login] = useMutation(LOGIN);
+    var obj :any;
     const navigate = useNavigate();
     const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         const name  = e.currentTarget.name;
@@ -26,65 +26,61 @@ const LandingPage = () => {
         if (name === 'email') setEmail(value);
         if (name === 'password') setPassword(value);
     };
-    if(!loadingLogin && !loadingRegister){
-        return(  
-            <>
-                <Container>
-                    <Header>
-                        <img src={Logo} alt="IEPAM LOGO"/>
-                    </Header>
 
-                        <Email>
-                            <input type="text"
-                                value={email}
-                                name='email'
-                                onChange={handleInput} 
-                                placeholder="Correo electronico"/>
-                        </Email>
+    return(  
+        <>
+            <Container>
+                <Header>
+                    <img src={Logo} alt="IEPAM LOGO"/>
+                </Header>
 
-                        <Password>
-                            <input type="password"
-                                value={password}
-                                name='password'
-                                onChange={handleInput} 
-                                placeholder="Contraseña"/>
-                        </Password>
+                    <Email>
+                        <input type="text"
+                            value={email}
+                            name='email'
+                            onChange={handleInput} 
+                            placeholder="Correo electronico"/>
+                    </Email>
 
-                        <ButtonsBox>
-                            <form
-                                onSubmit={e => {
-                                e.preventDefault();
-                                login({ variables: { email: email, password: password } });
-                                console.log(dataLogin.login);
-                                if(dataLogin.login === "Champion :D"){
-                                    navigate('/unity');
-                                }
-                            }}>
-                                <LoginButton type="submit">INICIAR SESIÓN</LoginButton>
-                            </form>
-                            <Link to="forgotpassword">
-                                <ForgotPasswordButton>OLVIDE MI CONTRASEÑA</ForgotPasswordButton>
-                            </Link>
-                            <form
-                                onSubmit={e => {
-                                e.preventDefault();
-                                register({ variables: { email: email, password: password } })
-                                console.log(dataRegister.register);
-                                if(dataRegister.register === "Champion :D"){
-                                    navigate('/unity');
-                                }
-                            }}>
-                                <RegisterButton type="submit">REGISTRATE</RegisterButton>
-                            </form>
-                        </ButtonsBox>
-                    <HomeImg src ={Background} alt="background"/>
-                </Container>
-            </>
-        )
-    }
-    else{
-        return(<></>)
-    }
+                    <Password>
+                        <input type="password"
+                            value={password}
+                            name='password'
+                            onChange={handleInput} 
+                            placeholder="Contraseña"/>
+                    </Password>
+
+                    <ButtonsBox>
+                        <form
+                            onSubmit={e => {
+                            e.preventDefault();
+                            login({ variables: { email: email, password: password } }).then(data => obj = data).then( () =>{
+                            console.log(obj.data.login);
+                            if(obj.data.login === "Champion :D"){
+                                navigate('/unity');
+                            }});
+                        }}>
+                            <LoginButton type="submit">INICIAR SESIÓN</LoginButton>
+                        </form>
+                        <Link to="forgotpassword">
+                            <ForgotPasswordButton>OLVIDE MI CONTRASEÑA</ForgotPasswordButton>
+                        </Link>
+                        <form
+                            onSubmit={e => {
+                            e.preventDefault();
+                            register({ variables: { email: email, password: password } }).then(data => obj = data).then( () =>{
+                            console.log(obj.data.register);
+                            if(obj.data.register === "Champion :D"){
+                                navigate('/unity');
+                            }});
+                        }}>
+                            <RegisterButton type="submit">REGISTRATE</RegisterButton>
+                        </form>
+                    </ButtonsBox>
+                <HomeImg src ={Background} alt="background"/>
+            </Container>
+        </>
+    )
 };
 
 export default LandingPage;
